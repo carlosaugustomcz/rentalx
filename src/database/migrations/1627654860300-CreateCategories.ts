6665192b94c779bcf1a7a751ebe989e93b2156cc
carlos.augustomcz <carlos.augustomcz@gmail.com>
2021-07-30 12:55:40 -0300

    criacao migrations

---
+++ src/database/migrations/1627654860300-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCategories1627654860300 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
